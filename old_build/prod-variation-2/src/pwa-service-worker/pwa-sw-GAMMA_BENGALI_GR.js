7e26ea0a21f395c0a84803d034adb8cf33c30063
kaynite <sachin@pratilipi.com>
2018-05-23 19:32:55 +0530

    added delta to old build

--- old_build/prod-variation-2/src/pwa-service-worker/pwa-sw-GAMMA_BENGALI_GR.js
+++ old_build/prod-variation-2/src/pwa-service-worker/pwa-sw-GAMMA_BENGALI_GR.js
@@ -4,7 +4,7 @@
 
 	var localFilesToCache = [
 		'.',
-		'pwa-stylesheets/css/style.css?070520181801',
+		'pwa-stylesheets/css/style.css?230520181903',
 		'pwa-images/404.svg',
 		'pwa-images/library-empty.svg',
 		'pwa-images/NewSprite_2.png',
@@ -29,7 +29,7 @@
 		'https://www.ptlp.co/resource-all/font/font-bn.css'
 	];
 
-	var STATIC_VERSION = "070520181801";
+	var STATIC_VERSION = "230520181903";
 	var DYNAMIC_VERSION = "7";
 	var staticCacheName = 'pratilipi-cache-static-' + STATIC_VERSION;
 	var dynamicCacheName = 'pratilipi-cache-dynamic-' + DYNAMIC_VERSION;
@@ -38,7 +38,7 @@
 	var apiPrefix = "https://bengali-gamma-gr.pratilipi.com";
 
 	/* Cache Keys */
-	var PWA_INDEX_HTML = "app-shell-070520181801.html";
+	var PWA_INDEX_HTML = "app-shell-230520181903.html";
 	var INIT_BANNER_LIST = "init-banner-list.json";
 	var TRENDING_SEARCH_KEYWORDS = "trending-search-keywords.json";
 
@@ -171,13 +171,22 @@
 	});
 
 	self.addEventListener( 'push', function(event) {
-		const title = 'Test Title';
-		const options = {
-			body: 'Test body....'
-		};
 
-		const notificationPromise = self.registration.showNotification(title, options);
-		event.waitUntil( notificationPromise );
+		// Implementation => Only for marketing notifications.
+		var pratilipi = event.data.json();
+
+		if( pratilipi ) {
+			var title = pratilipi.displayTitle,
+				options = {
+					badge: 'https://storage.googleapis.com/public.pratilipi.com/pratilipi-logo/png/ic_launcher.png',
+					body: pratilipi.summary,
+					icon: 'https://storage.googleapis.com/public.pratilipi.com/pratilipi-logo/png/Logo-2C-RGB-200px.png', 
+					// image: pratilipi.coverImageUrl,
+					data: pratilipi // on notificationclick event
+				};
+			var notificationPromise = self.registration.showNotification(title, options);
+			event.waitUntil( notificationPromise );
+		}
 
 	});
 
@@ -185,7 +194,7 @@
 	self.addEventListener( 'notificationclick', function(event) {
 		event.notification.close();
 		event.waitUntil(
-			clients.openWindow('https://www.pratilipi.com')
+			clients.openWindow(event.notification.data.pageUrl)
 		);
 	});
 
