d43e2a6d518a6df8c1fee94df971160dac57634c
kaynite <sachin@pratilipi.com>
2018-05-24 18:07:40 +0530

    changed build script

--- build.sh
+++ build.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
-declare -a process_arr_1=("LANGUAGE=te npm run build &" "LANGUAGE=bn npm run build &" "LANGUAGE=gu npm run build &" "LANGUAGE=hi npm run build &" "LANGUAGE=kn npm run build &" "LANGUAGE=ml npm run build &" "LANGUAGE=mr npm run build &" "LANGUAGE=ta npm run build &" )
+declare -a process_arr_1=("LANGUAGE=te npm run build &" "LANGUAGE=bn npm run build &" "LANGUAGE=gu npm run build &" "LANGUAGE=hi npm run build &" )
+                                                                                                                                                                                                                                                                                                                                declare -a process_arr_2=("LANGUAGE=kn npm run build &" "LANGUAGE=ml npm run build &" "LANGUAGE=mr npm run build &" "LANGUAGE=ta npm run build &" )
+
+#declare -a process_arr_1=("echo \" sachin 1\" " "echo \" sachin 2\" " "echo \" sachin 3\" ")
+#declare -a process_arr_2=("echo \" sachin 4 \" ")
+
+total_size=$((${#process_arr_1[@]}+${#process_arr_2[@]}))
 
 for (( i = 0; i < ${#process_arr_1[@]} ; i++ )); do
     printf "\n**** Running: ${process_arr_1[$i]} *****\n\n"
@@ -15,9 +21,16 @@ for (( i = 0; i < ${#pids[@]} ; i++ )); do
     if [ "$exit_code" -gt "0" ]
     then
         printf "\n error in ${pid} status code ${exit_code} \n"
-        printf "\n command  ${process_arr_1[$i]} \n"
         exit 1
     else
         printf "\n done with ${pid} status code ${exit_code} \n"
+        next_index=$((${#process_arr_1[@]}+${i}))
+        if [ "$next_index" -lt "${total_size}" ]
+        then
+            printf "\n**** Running: ${process_arr_2[$i]} *****\n\n"
+            eval "${process_arr_2[$i]}"
+            pids[${next_index}]=$!
+            printf "\n starting another process ${process_arr_1[$i]} \n"
+        fi
     fi
 done
