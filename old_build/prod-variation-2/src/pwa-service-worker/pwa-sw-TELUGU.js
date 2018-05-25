b6217c447fe1f9ba9b819565ce82980037f1c364
kaynite <sachin@pratilipi.com>
2018-05-25 18:48:11 +0530

    updating builds

--- old_build/prod-variation-2/src/pwa-service-worker/pwa-sw-TELUGU.js
+++ old_build/prod-variation-2/src/pwa-service-worker/pwa-sw-TELUGU.js
@@ -4,7 +4,7 @@
 
 	var localFilesToCache = [
 		'.',
-		'pwa-stylesheets/css/style.css?230520181903',
+		'pwa-stylesheets/css/style.css?250520181827',
 		'pwa-images/404.svg',
 		'pwa-images/library-empty.svg',
 		'pwa-images/NewSprite_2.png',
@@ -29,7 +29,7 @@
 		'https://www.ptlp.co/resource-all/font/font-te.css'
 	];
 
-	var STATIC_VERSION = "230520181903";
+	var STATIC_VERSION = "250520181827";
 	var DYNAMIC_VERSION = "7";
 	var staticCacheName = 'pratilipi-cache-static-' + STATIC_VERSION;
 	var dynamicCacheName = 'pratilipi-cache-dynamic-' + DYNAMIC_VERSION;
@@ -38,7 +38,7 @@
 	var apiPrefix = "https://telugu.pratilipi.com";
 
 	/* Cache Keys */
-	var PWA_INDEX_HTML = "app-shell-230520181903.html";
+	var PWA_INDEX_HTML = "app-shell-250520181827.html";
 	var INIT_BANNER_LIST = "init-banner-list.json";
 	var TRENDING_SEARCH_KEYWORDS = "trending-search-keywords.json";
 
