06b1b9539c85cb58b3b2a8cd590ad6e7b8a61f5f
Suryadeep Pal <suryadeep10@gmail.com>
2018-07-06 17:12:12 +0530

    [FIX] Fix for build script

--- build/build_prod.sh
+++ build/build_prod.sh
@@ -2,14 +2,14 @@ git clone https://github.com/Pratilipi-Growth/ecs-mark-8.git
 cd ecs-mark-8
 npm install
 
-LANGUAGE=hi npm run build
-LANGUAGE=bn npm run build
-LANGUAGE=kn npm run build
-LANGUAGE=ta npm run build
-LANGUAGE=te npm run build
-LANGUAGE=mr npm run build
-LANGUAGE=ml npm run build
-LANGUAGE=gu npm run build
+REALM=PROD LANGUAGE=hi npm run build
+REALM=PROD LANGUAGE=bn npm run build
+REALM=PROD LANGUAGE=kn npm run build
+REALM=PROD LANGUAGE=ta npm run build
+REALM=PROD LANGUAGE=te npm run build
+REALM=PROD LANGUAGE=mr npm run build
+REALM=PROD LANGUAGE=ml npm run build
+REALM=PROD LANGUAGE=gu npm run build
 
 cp -Rv ./dist/hi ./../old_build/
 cp -Rv ./dist/bn ./../old_build/
